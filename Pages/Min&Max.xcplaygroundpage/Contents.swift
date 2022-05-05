2abae591eb01a2653e132b081c3ec68d6a696125
Harry-KNIGHT <eliott.knight@icloud.com>
2022-05-05 15:25:32 +0200

    Minimum to maximum

---
+++ Pages/Min&Max.xcplaygroundpage/Contents.swift
@@ -0,0 +1,43 @@
+//: [Previous](@previous)
+
+import Foundation
+// No error, basic func breakable 
+func minToMax(_ min: Int, max: Int) -> String {
+    var number = min
+    var numbers = ""
+
+
+    while number <= max {
+        numbers += String(number) + " "
+         number += 1
+    }
+    return numbers.trimmingCharacters(in: .whitespacesAndNewlines)
+}
+
+print(minToMax(10, max: 15))
+//: [Next](@next)
+
+// Function with error handling
+func minimumToMaximum(_ min: String, max: String) -> String {
+    var midNumbers = ""
+    var mini: Int = Int(min) ?? 0
+    let maxi: Int = Int(max) ?? 0
+    
+    for i in min {
+            for x in max {
+                if i.isNumber && x.isNumber {
+                    while mini <= maxi {
+                        midNumbers.append(String(mini) + " ")
+                        mini += 1
+                    }
+                }
+                return "error"
+            }
+        }
+    return midNumbers
+}
+
+print(minimumToMaximum("10", max: "20"))
+
+
+
