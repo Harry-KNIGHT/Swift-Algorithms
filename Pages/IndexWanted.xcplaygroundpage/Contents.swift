cab470f67342bf0e76923c1d4420b05fbe177403
Harry-KNIGHT <eliott.knight@icloud.com>
2022-05-06 20:37:56 +0200

    Index wanted algorithm.

---
+++ Pages/IndexWanted.xcplaygroundpage/Contents.swift
@@ -0,0 +1,40 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+    Créez un programme qui affiche le premier index d’un élément
+    recherché dans un tableau.
+ 
+    Le tableau est constitué de tous les arguments sauf le dernier.
+
+    L’élément recherché est le dernier argument.
+ 
+    Afficher -1 si l’élément n’est pas trouvé.
+ */
+
+func indexWanted(_ entry: String) -> String {
+    // splitted in an array
+    let splitted = entry.split(separator: " ")
+    
+    // Count how many string is equal to the last one in array
+    var filterArray = splitted.filter { $0 == splitted.last }.count
+    
+    //Error Handling
+    if entry.isEmpty {
+        return "Error"
+    }else if filterArray < 2 {
+        return "-1"
+    }else {
+        // If no error, then loop inside array
+        for (index, char) in splitted.enumerated() {
+           if char == splitted.last {
+                return String(index)
+           }
+        }
+    }
+    return "Something went wrong please try again"
+}
+print(indexWanted("JE SUIS UN SOLDAT"))
+
+//: [Next](@next)
