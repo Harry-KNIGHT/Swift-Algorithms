9dc4d77595747b76983ce9136f94a57ac2bb0064
Harry-KNIGHT <eliott.knight@icloud.com>
2022-05-08 09:35:45 +0200

    Loop performance accured removed line without utility.

--- Pages/EvenOrOdd.xcplaygroundpage/Contents.swift
+++ Pages/EvenOrOdd.xcplaygroundpage/Contents.swift
@@ -5,34 +5,33 @@ import Foundation
 func evenOrOddFunc(numberInt: Int) -> String {
     if numberInt < 0 {
         return "Negative number not accepted"
-    } else {
-        if numberInt.isMultiple(of: 2) {
-            return "pair"
-        }else if numberInt.isMultiple(of: 1) {
-            return "impair"
-        }else {
-            return "error"
-        }
+    }
+    if numberInt.isMultiple(of: 2) {
+        return "pair"
+    }else if numberInt.isMultiple(of: 1) {
+        return "impair"
+    }else {
+        return "error"
     }
 }
 
 print(evenOrOddFunc(numberInt: 10))
 
+// Error handling with string argument as shell command line parser
 func isEvenNumber(_ number: String) -> String {
     let num = Int(number) ?? 0
     
     for i in number {
         if !i.isNumber {
             return "Error"
+        }
+        if num % 2 == 0 {
+            return "Pair"
         }else {
-            if num.isMultiple(of: 2) {
-                return "Pair"
-            }else {
-                return "Impair"
-            }
+            return "Impair"
         }
     }
     return "Something went wrong"
 }
-print(isEvenNumber("43"))
+print(isEvenNumber("22"))
 //: [Next](@next)
