403d3e549675ceb68acb8d086330eeaf0e654ccb
Harry-KNIGHT <eliott.knight@icloud.com>
2022-05-08 14:36:24 +0200

    [Fix] Crash if number is divided by 0.

--- Pages/Divisions.xcplaygroundpage/Contents.swift
+++ Pages/Divisions.xcplaygroundpage/Contents.swift
@@ -4,20 +4,22 @@ import Foundation
 
 /*
  Créez un programme qui affiche le résultat et le reste d’une division entre deux nombres.
-*/
+ */
 
 func division(_ number: String, divisedBy: String) -> String {
     let number = Int(number) ?? 0
     let divisedBy = Int(divisedBy) ?? 0
     
+    //Error handling
+    if divisedBy == 0 || divisedBy > number {
+        return "Error"
+    }
+    //If no error, then divide and take the rest.
     let result = number / divisedBy
     let rest = divisedBy / divisedBy
     
-    if divisedBy > number || divisedBy == 0 {
-        return "Error"
-    }
     return "Resultat: \(result) \nReste: \(rest)"
 }
 
-print(division("5", divisedBy: "2"))
+print(division("5", divisedBy: "0"))
 //: [Next](@next)
